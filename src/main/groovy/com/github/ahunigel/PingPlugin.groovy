350662fc98525666ce16f1b39dbd60f5b9207eba
Nigel.Zheng <nigel.zheng@emerson.com>
2019-05-06 00:51:10 +0800

    feat: be able to toggleOfflineMode; be able to ping before resolve configurations

--- src/main/groovy/com/github/ahunigel/PingPlugin.groovy
+++ src/main/groovy/com/github/ahunigel/PingPlugin.groovy
@@ -5,6 +5,8 @@ import org.gradle.api.Project
 
 class PingPlugin implements Plugin<Project> {
 
+    private pingStatusDetermined = false
+
     @Override
     void apply(Project project) {
 
@@ -16,20 +18,32 @@ class PingPlugin implements Plugin<Project> {
             }
         }
         // project.getTasks().getByName('clean').dependsOn('ping')
+        project.configurations.each {
+            it.incoming.beforeResolve {
+                if (!pingStatusDetermined) {
+                    ping(project)
+                    pingStatusDetermined = true
+                }
+            }
+        }
     }
 
     void ping(Project project) {
         def baseHost = project.ping.host
         def pingTimeout = project.ping.timeout
+        def pingToggleOfflineMode = project.ping.toggleOfflineMode
         try {
             if (InetAddress.getByName(baseHost).isReachable(pingTimeout)) {
                 project.ping.reachable = true
+                toggleOfflineMode(project, pingToggleOfflineMode, false)
                 println "Host ${baseHost} is reachable"
             } else {
+                toggleOfflineMode(project, pingToggleOfflineMode, true)
                 println "Host ${baseHost} is not reachable"
             }
         } catch (UnknownHostException e) {
             println "Unknown host ${baseHost}"
+            toggleOfflineMode(project, pingToggleOfflineMode, true)
             return
         } catch (all) {
             println "Host ${baseHost} is not reachable, beacuse ${all}"
@@ -42,10 +56,18 @@ class PingPlugin implements Plugin<Project> {
                     commandLine 'cmd', '/c', "ping ${baseHost}"
                 }
                 project.ping.reachable = true
+                toggleOfflineMode(project, pingToggleOfflineMode, false)
             } catch (all) {
+                toggleOfflineMode(project, pingToggleOfflineMode, true)
                 println "Host ${baseHost} is not reachable, beacuse ${all}"
             }
         }
         project.logger.info "Host ${baseHost} reachable is ${project.ping.reachable}"
+    }
+
+    void toggleOfflineMode(Project project, boolean pingToggleOfflineMode, boolean toOfflineMode) {
+        if (pingToggleOfflineMode && project.gradle.startParameter.offline != toOfflineMode) {
+            project.gradle.startParameter.offline = toOfflineMode
+        }
     }
 }
